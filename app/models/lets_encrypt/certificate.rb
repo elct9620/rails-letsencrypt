6a605ca829a5b818e96302509013f72c74709bb5
蒼時弦也 <elct9620@frost.tw>
2017-05-11 16:49:09 +0800

    Add more helper method for LetsEncrypt::Certificate

--- app/models/lets_encrypt/certificate.rb
+++ app/models/lets_encrypt/certificate.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module LetsEncrypt
   # :nodoc:
   class Certificate < ActiveRecord::Base
@@ -6,15 +8,37 @@ module LetsEncrypt
 
     validates :domain, presence: true, uniqueness: true
 
+    scope :active, -> { where('certificate IS NOT NULL AND expires_at > ?', Time.zone.now) }
+    scope :renewable, -> { where('renew_after IS NULL OR renew_after <= ?', Time.zone.now) }
+    scope :expired, -> { where('expires_at <= ?', Time.zone.now) }
+
     before_create -> { self.key = OpenSSL::PKey::RSA.new(4096).to_s }
     after_save -> { save_to_redis }, if: -> { LetsEncrypt.config.use_redis? }
 
-    def get
-      verify && issue
+    def active?
+      certificate.present?
     end
+
+    def expired?
+      Time.zone.now >= expires_at
+    end
+
+    def get
+      verif && issue
+    end
+
+    alias renew get
 
     def bundle
       [intermediaries, certificate].join("\n")
+    end
+
+    def certificate_object
+      @certificate_object ||= OpenSSL::X509::Certificate.new(certificate)
+    end
+
+    def key_object
+      @key_object ||= OpenSSL::PKey::RSA.new(key)
     end
 
     def save_to_redis
