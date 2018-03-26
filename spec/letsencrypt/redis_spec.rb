76593c62bae2f56286061d7fbd74104958fa7d33
Nathan Broadbent <git@ndbroadbent.com>
2018-03-27 00:32:13 +0700

    Don't save blank certificates/keys to Redis

--- spec/letsencrypt/redis_spec.rb
+++ spec/letsencrypt/redis_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails-letsencrypt'
+require 'rails_helper'
 
 RSpec.describe LetsEncrypt::Redis do
   let(:redis) { double(::Redis) }
@@ -15,8 +15,16 @@ RSpec.describe LetsEncrypt::Redis do
 
   describe '#save' do
     it 'saves certificate into redis' do
+      certificate.key = "KEY"
+      certificate.certificate = "CERTIFICATE"
       expect(redis).to receive(:set).with("#{domain}.key", an_instance_of(String))
       expect(redis).to receive(:set).with("#{domain}.crt", an_instance_of(String))
+      LetsEncrypt::Redis.save(certificate)
+    end
+
+    it 'doesnt save blank certificate into redis' do
+      expect(redis).to_not receive(:set).with("#{domain}.key", an_instance_of(String))
+      expect(redis).to_not receive(:set).with("#{domain}.crt", an_instance_of(String))
       LetsEncrypt::Redis.save(certificate)
     end
   end
