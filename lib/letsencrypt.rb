91dec80e0f50962501a9f478c931007fe559a5a4
vegeta <phyala@gmail.com>
2018-05-11 14:02:05 +0800

    Recover endpoint code in letsencrypt client

--- lib/letsencrypt.rb
+++ lib/letsencrypt.rb
@@ -21,10 +21,9 @@ module LetsEncrypt
   class << self
     # Create the ACME Client to Let's Encrypt
     def client
-      @endpoint ||= config.use_staging? ? ENDPOINT_STAGING : ENDPOINT
       @client ||= ::Acme::Client.new(
         private_key: private_key,
-        endpoint: @endpoint
+        endpoint: endpoint
       )
     end
 
