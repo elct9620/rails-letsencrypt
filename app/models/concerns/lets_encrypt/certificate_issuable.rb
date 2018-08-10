d961b6098f24f69766c065f8d49f30af8c69b2a4
蒼時弦也 <elct9620@frost.tw>
2018-08-10 16:34:03 +0800

    Add fullchain support

--- app/models/concerns/lets_encrypt/certificate_issuable.rb
+++ app/models/concerns/lets_encrypt/certificate_issuable.rb
@@ -29,10 +29,10 @@ module LetsEncrypt
     def create_certificate
       order.finalize(csr: csr)
       sleep 1 while order.status == 'processing'
-      cert = OpenSSL::X509::Certificate.new(order.certificate)
+      fullchain = order.certificate.split("\n\n")
+      cert = OpenSSL::X509::Certificate.new(fullchain.shift)
       self.certificate = cert.to_pem
-      # TODO: Provide fullchain support
-      # self.intermediaries = https_cert.chain_to_pem
+      self.intermediaries = fullchain.join("\n\n")
       self.expires_at = cert.not_after
       self.renew_after = (expires_at - 1.month) + rand(10).days
       save!
