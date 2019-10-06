80918bd1a1281a1522a5666eb0bfde6aac660399
telsaiori <telsaiori@gmail.com>
2019-10-06 23:52:13 +0800

    Add test to check controller will work properly with default model and customize model

--- spec/controllers/lets_encrypt/verifications_controller_spec.rb
+++ spec/controllers/lets_encrypt/verifications_controller_spec.rb
@@ -6,28 +6,40 @@ RSpec.describe LetsEncrypt::VerificationsController, type: :controller do
   routes { LetsEncrypt::Engine.routes }
   class OtherModel < LetsEncrypt::Certificate
   end
-  before(:each) do
-    LetsEncrypt.config.certificate_model = 'OtherModel'
-  end
 
   it 'returns 404 status when no valid verification path found' do
     open(:get, :show, verification_path: :invalid_path)
     expect(response.status).to eq(404)
   end
 
-  context 'has certificate' do
-    let!(:certificate) do
+
+  describe 'has certificate' do
+   let!(:certificate) do
       LetsEncrypt.certificate_model.create(
         domain: 'example.com',
         verification_path: '.well-known/acme-challenge/valid_path',
         verification_string: 'verification'
       )
     end
+    context 'with default model' do
+      it 'returns verification string when found verification path' do
+        open(:get, :show, verification_path: 'valid_path')
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(certificate.verification_string)
+      end
+    end
 
-    it 'returns verification string when found verification path' do
-      open(:get, :show, verification_path: 'valid_path')
-      expect(response.status).to eq(200)
-      expect(response.body).to eq(certificate.verification_string)
+    context 'with customize model' do
+      LetsEncrypt.config.certificate_model = 'OtherModel'
+      it 'set the certificate_model to customize model' do
+        expect(LetsEncrypt.certificate_model).to eq('OtherModel'.constantize)
+      end
+
+      it 'returns verification string when found verification path' do
+        open(:get, :show, verification_path: 'valid_path')
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(certificate.verification_string)
+      end
     end
   end
 end
